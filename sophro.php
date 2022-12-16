837c7cdf3b409c60057d65eca662facc2311e5a6
olivier_dufour <olivier_dufour@ens.univ-artois.fr>
2022-12-16 21:32:00 +0100

    sophro page ok

--- sophro.php
+++ sophro.php
@@ -32,6 +32,9 @@
         </div>
     </section>
     <section id="dif" class="background-brown col-12">
+        <div class="col-12 text-center pt-5">
+            <h2>Les différentes influences de la sophrologie</h2>
+        </div>
         <div id="grid1" class="grid-1 col-10 mx-auto">
             <div class="col-8 bg-grey mx-auto text-center mt-5 border shadow-lg p-2">
                 <h2>Neurologie contemporaine</h2>
@@ -108,7 +111,106 @@
         <div class="col-12 pb-4">
             <div class="col-6 mx-auto">
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores similique id aliquam officiis a quaerat quos ad autem, voluptas ducimus magni eaque doloribus saepe consequuntur, tempore iusto mollitia error.
-                Nobis blanditiis quasi consequuntur provident. Eligendi, cupiditate obcaecati ex ipsa debitis architecto similique dolore dolores rerum, natus ducimus ullam earum eveniet est enim corrupti eum voluptate placeat, nulla alias. Recusandae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quod amet nam. Est consequuntur atque voluptates voluptatum ratione eius eveniet minima ab? Molestiae debitis quo ea, sint pariatur aut id.</p>
+                    Nobis blanditiis quasi consequuntur provident. Eligendi, cupiditate obcaecati ex ipsa debitis architecto similique dolore dolores rerum, natus ducimus ullam earum eveniet est enim corrupti eum voluptate placeat, nulla alias. Recusandae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quod amet nam. Est consequuntur atque voluptates voluptatum ratione eius eveniet minima ab? Molestiae debitis quo ea, sint pariatur aut id.</p>
+            </div>
+        </div>
+    </section>
+    <section id="dif2" class="col-12">
+        <div class="col-12 text-center pt-5">
+            <h2>Les champs d'applications de la sophroligie</h2>
+        </div>
+        <div id="grid1" class="grid-1 col-10 mx-auto">
+            <div class="col-8 bg-grey mx-auto text-center mt-5 border shadow-lg p-2">
+                <h2>Santé</h2>
+                <p>Maladie</p>
+                <p>Cancer</p>
+                <p>Soin de support</p>
+                <p>Poids</p>
+                <p>Alimentation</p>
+            </div>
+            <div class="col-8 bg-grey mx-auto text-center mt-5 border shadow-lg p-2">
+                <h2>Développement personnel</h2>
+                <p>Capacités</p>
+                <p>Espoirs</p>
+                <p>Désirs</p>
+                <p>Redéploiement existentiel</p>
+                <p>Estime de soi</p>
+            </div>
+            <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg p-2 mt-5">
+                <h2>Environnement</h2>
+                <p>Adaptabilité</p>
+                <p>Écologie de la personne</p>
+                <p>Forme</p>
+                <p>Performance</p>
+                <p>Récupération</p>
+            </div>
+
+        </div>
+        <div id="grid3" class="grid-1 col-10 mx-auto mt-5 mb-5">
+            <div class="col-12 d-flex align-items-center justify-content-center flex-column mx-auto">
+                <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg mt-3 p-2">
+                    <h2>Enfant</h2>
+                    <p>Scolarité</p>
+                    <p>Concentration</p>
+                    <p>Dyslexie</p>
+                    <p>Écologie familiale</p>
+                </div>
+                <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg mt-5 p-2">
+                    <h2>Sport</h2>
+                    <p>Entraînnement</p>
+                    <p>Préparation</p>
+                    <p>Forme</p>
+                    <p>Performance</p>
+                    <p>Récupération</p>
+                </div>
+            </div>
+            <div class="col-9 mx-auto my-auto">
+                <img src="assets/img/FB_IMG_1669483855666.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-12 d-flex align-items-center justify-content-center flex-column mx-auto">
+                <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg mt-3 p-2">
+                    <h2>Hygiène de vie</h2>
+                    <p>Pression du temps</p>
+                    <p>Mode de vie</p>
+                    <p>Sommeil</p>
+                    <p>Sédentarité</p>
+                </div>
+                <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg mt-5 p-2">
+                    <h2>Famille</h2>
+                    <p>Couple</p>
+                    <p>Conflits</p>
+                    <p>Séparations</p>
+                    <p>Sexualité</p>
+                    <p>Mariage</p>
+
+                </div>
+            </div>
+        </div>
+        <div id="grid3" class="grid-1 col-10 mx-auto">
+            <div class="col-8 bg-grey mx-auto text-center mb-5 border shadow-lg p-2">
+                <h2>Addiction</h2>
+                <p>Tabac</p>
+                <p>Drogues</p>
+                <p>Alcool</p>
+                <p>Jeux vidéos</p>
+                <p>etc</p>
+            </div>
+            <div class="col-8 bg-grey mx-auto text-center mb-5 border shadow-lg p-2">
+                <h2>Handicap</h2>
+                <p>Autonomie</p>
+            </div>
+            <div class="col-8 bg-grey mx-auto text-center mb-5 border shadow-lg p-2">
+                <h2>Profession</h2>
+                <p>Insatisfactions</p>
+                <p>Relationnelles</p>
+                <p>Bien-être au travail</p>
+                <p>Burn-out et bore-out</p>
+            </div>
+        </div>
+        <div class="col-12 pb-4">
+            <div class="col-6 mx-auto">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores similique id aliquam officiis a quaerat quos ad autem, voluptas ducimus magni eaque doloribus saepe consequuntur, tempore iusto mollitia error.
+                    Nobis blanditiis quasi consequuntur provident. Eligendi, cupiditate obcaecati ex ipsa debitis architecto similique dolore dolores rerum, natus ducimus ullam earum eveniet est enim corrupti eum voluptate placeat, nulla alias. Recusandae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quod amet nam. Est consequuntur atque voluptates voluptatum ratione eius eveniet minima ab? Molestiae debitis quo ea, sint pariatur aut id.</p>
             </div>
         </div>
     </section>
