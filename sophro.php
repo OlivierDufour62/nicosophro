36b766453d21370ba995e1509af786fd1f8a093a
olivier_dufour <olivier_dufour@ens.univ-artois.fr>
2022-12-16 19:21:38 +0100

    card sophro

--- sophro.php
+++ sophro.php
@@ -22,55 +22,93 @@
         <div class="text-center mt-5 mb-5">
             <h1>La Sophrologie</h1>
         </div>
-            <div class="row col-12 d-flex justify-content-center mb-5">
-                <div class="col-5">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, possimus eius provident, voluptatibus cupiditate placeat maxime omnis aut quod veniam nostrum incidunt consectetur voluptatum corrupti ut officia vitae dolorum animi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facilis, deleniti assumenda iure culpa eaque rerum explicabo ut numquam rem doloremque quasi exercitationem sint magnam voluptates reprehenderit blanditiis labore nihil!</p>
-                </div>
-                <div class="col-1">
-                    <img src="assets/img/alphonso.jpg" alt="Alphonso Caicedo" class="img-fluid">
-                </div>
+        <div class="row col-12 d-flex justify-content-center mb-5">
+            <div class="col-5">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, possimus eius provident, voluptatibus cupiditate placeat maxime omnis aut quod veniam nostrum incidunt consectetur voluptatum corrupti ut officia vitae dolorum animi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex facilis, deleniti assumenda iure culpa eaque rerum explicabo ut numquam rem doloremque quasi exercitationem sint magnam voluptates reprehenderit blanditiis labore nihil!</p>
             </div>
-    </section>
-    <section class="background-brown">
-        <div id="grid1" class="grid-1 col-8">
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
-            </div>
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
-            </div>
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
+            <div class="col-1">
+                <img src="assets/img/alphonso.jpg" alt="Alphonso Caicedo" class="img-fluid">
             </div>
         </div>
-        <div id="grid3" class="grid-1 col-8">
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
+    </section>
+    <section id="dif" class="background-brown col-12">
+        <div id="grid1" class="grid-1 col-10 mx-auto">
+            <div class="col-8 bg-grey mx-auto text-center mt-5 border shadow-lg p-2">
+                <h2>Neurologie contemporaine</h2>
+                <div class="d-flex">
+                    <div class="mx-auto">
+                        <p>E.E.G</p>
+                        <p>Vigilance</p>
+                        <p>Réflexes</p>
+                        <p>Schéma corporel</p>
+                    </div>
+                    <div class="mx-auto">
+                        <p>Systéme réticulaire</p>
+                        <p>Anatomie S.N.C</p>
+                        <p>Neurosciences</p>
+                        <p>Neurogénes</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-5">
+            <div class="col-8 bg-grey mx-auto text-center mt-5 border shadow-lg p-2">
+                <h2>Psychiatrie phenoménologique existentielle</h2>
+                <p>Binswanger</p>
+                <p>Minkowski</p>
+                <p>Lopez Ibor</p>
+            </div>
+            <div class="col-8 bg-grey mx-auto text-center mt-5 border shadow-lg p-2">
+                <h2>Phylosophie phenoménologique</h2>
+                <p>Hegel</p>
+                <p>Husserl</p>
+                <p>Heidegger</p>
+                <p>Merleau-Ponty</p>
+                <p>Scheller</p>
+            </div>
+        </div>
+        <div id="grid3" class="grid-1 col-10 mx-auto mt-5 mb-5">
+            <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg">
+                <h2>Hypnose</h2>
+                <p>Messmer</p>
+                <p>Bernheim</p>
+                <p>Charcot</p>
+                <p>Liebault</p>
+                <p>Érikson</p>
+            </div>
+            <div class="col-9 mx-auto border shadow-lg">
                 <img src="assets/img/FB_IMG_1669483855666.jpg" alt="" class="img-fluid">
             </div>
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
+            <div class="col-8 bg-grey d-flex align-items-center justify-content-center flex-column mx-auto border shadow-lg">
+                <h2>Gréce classique</h2>
+                <p>Platon</p>
+                <p>Hippocrate</p>
+                <p>Aristote</p>
+                <p>Sémantique</p>
             </div>
         </div>
-        <div id="grid3" class="grid-1 col-8">
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
+        <div id="grid3" class="grid-1 col-10 mx-auto">
+            <div class="col-8 bg-grey mx-auto text-center mb-5 border shadow-lg p-2">
+                <h2>Psychologie</h2>
+                <p>Dilthey</p>
+                <p>Jaspers</p>
+                <p>Freud et Jung</p>
+                <p>Résilience</p>
             </div>
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
+            <div class="col-8 bg-grey mx-auto text-center mb-5 border shadow-lg p-2">
+                <h2>Orient</h2>
+                <p>Yoga</p>
+                <p>Bouddhisme</p>
+                <p>Zen</p>
             </div>
-            <div class="col-3">
-                <p>coucou</p>
-                <p>coucou</p>
+            <div class="col-8 bg-grey mx-auto text-center mb-5 border shadow-lg p-2">
+                <h2>Relaxation</h2>
+                <p>Schultz</p>
+                <p>Jacobson</p>
+            </div>
+        </div>
+        <div class="col-12 pb-4">
+            <div class="col-6 mx-auto">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolores similique id aliquam officiis a quaerat quos ad autem, voluptas ducimus magni eaque doloribus saepe consequuntur, tempore iusto mollitia error.
+                Nobis blanditiis quasi consequuntur provident. Eligendi, cupiditate obcaecati ex ipsa debitis architecto similique dolore dolores rerum, natus ducimus ullam earum eveniet est enim corrupti eum voluptate placeat, nulla alias. Recusandae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quod amet nam. Est consequuntur atque voluptates voluptatum ratione eius eveniet minima ab? Molestiae debitis quo ea, sint pariatur aut id.</p>
             </div>
         </div>
     </section>
